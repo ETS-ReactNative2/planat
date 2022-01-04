46f213c064eff7bfac1e0e3490a8dc967c9119c2
Izabela <81172851+Izabella99@users.noreply.github.com>
2022-01-04 18:52:03 +0200

    Add files via upload

---
+++ build/precache-manifest.fc81dd80ad68c490e90d549129885cf1.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e4611b9a1e7245b89504d1242c7b47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b359fc95ced61f585ead",
+    "url": "/static/css/2.6dacf5a8.chunk.css"
+  },
+  {
+    "revision": "76af7a50b0be54fa1170",
+    "url": "/static/css/main.2f4af40d.chunk.css"
+  },
+  {
+    "revision": "b359fc95ced61f585ead",
+    "url": "/static/js/2.a4ce8643.chunk.js"
+  },
+  {
+    "revision": "42b088b955f7447e1f44c7dcc5a1962d",
+    "url": "/static/js/2.a4ce8643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76af7a50b0be54fa1170",
+    "url": "/static/js/main.99b6b62c.chunk.js"
+  },
+  {
+    "revision": "0a6b3e236a7baddd3934",
+    "url": "/static/js/runtime-main.2511424e.js"
+  },
+  {
+    "revision": "e4da3480b63c1d8c74430a053c3d2faf",
+    "url": "/static/media/Area47.e4da3480.png"
+  },
+  {
+    "revision": "13b0a216d47454dc691771f21ec9b7a8",
+    "url": "/static/media/BurjKhalifa.13b0a216.png"
+  },
+  {
+    "revision": "45346808fe82e71174eddbb51457165f",
+    "url": "/static/media/GrandCanyon.45346808.png"
+  },
+  {
+    "revision": "7f52fe1272e27dfdab7222562ed59ccf",
+    "url": "/static/media/Louvre.7f52fe12.png"
+  },
+  {
+    "revision": "94c72b9981a40a4be2995fa4fb60c355",
+    "url": "/static/media/NewYork.94c72b99.png"
+  },
+  {
+    "revision": "d7b6bc08a3f2b295dea1671d1388dd60",
+    "url": "/static/media/Pyramid.d7b6bc08.png"
+  },
+  {
+    "revision": "c27d4c07cb28201083dd7b9afdda8120",
+    "url": "/static/media/Robot.c27d4c07.png"
+  },
+  {
+    "revision": "ccc35c3f974239e93244d76b628b3aeb",
+    "url": "/static/media/arrow.ccc35c3f.svg"
+  },
+  {
+    "revision": "1e7d8675d52754d3fd7a27c08529850f",
+    "url": "/static/media/image-placeholder.1e7d8675.svg"
+  },
+  {
+    "revision": "f0bc1707f4732488d6c0d1786023d94c",
+    "url": "/static/media/marker.f0bc1707.png"
+  },
+  {
+    "revision": "44da0c1e7dea7b2f6484d8821685f718",
+    "url": "/static/media/pin.44da0c1e.svg"
+  },
+  {
+    "revision": "6987b4ac0fbad21acff302235fbe70d7",
+    "url": "/static/media/planat_logo.6987b4ac.png"
+  },
+  {
+    "revision": "40ea824075d5988daf6b34446652fb5c",
+    "url": "/static/media/right-arrow.40ea8240.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
